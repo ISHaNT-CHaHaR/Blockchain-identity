f0abdc0bca36b65ce7f7b9b78e284ad7674b5e0a
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-04-30 22:49:12 +0530

    added hashing function.

--- newfile.js
+++ newfile.js
@@ -1,1 +1,15 @@
-console.log("Yay! I'm Enjoying this bruh!")
+const crypto = require('crypto');
+
+let data = 'Blockchain Rock!';
+let obj = {
+   id: 1,
+   body: 'With Object Works too',
+   time: new Date().getTime().toString().slice(0, -3),
+};
+
+function hash(obj) {
+   obj = JSON.stringify(obj);
+   return crypto.createHash('sha256').update(obj).digest('hex');
+}
+console.log(obj);
+console.log(hash(obj));
