f4245e7f734d316ebbf644e9593a6317146d66b5
ISHaNT-CHaHaR <56401920+ISHaNT-CHaHaR@users.noreply.github.com>
2020-05-04 15:05:30 +0530

    Created a private Blockchain sample

--- simpleChain.js
+++ simpleChain.js
@@ -1,16 +1,23 @@
+/* ===== SHA256 with Crypto-js ===================================
+|  Learn more: Crypto-js: https://github.com/brix/crypto-js      |
+|  =============================================================*/
+
+const SHA256 = require('crypto-js/sha256');
+
+/* ===== Block Class ===================================
+|  Class with a constructor for block data model       |
+|  ====================================================*/
+
 class Block {
    constructor(data) {
-      /// Blockdatamodel
-
-      this.height = 0;
+      this.height = '';
+      this.timeStamp = '';
       this.data = data;
-      this.time = 0;
-      this.previousblockhash = '';
+      this.previousHash = '0x';
       this.hash = '';
    }
 }
 
-///
 /* ===== Blockchain ===================================
 |  Class with a constructor for blockchain data model  |
 |  with functions to support:                          |
@@ -24,10 +31,35 @@ class Block {
 
 class Blockchain {
    constructor() {
+      // new chain array
       this.chain = [];
+      // add first genesis block
+      this.addBlock(this.createGenesisBlock());
    }
 
+   createGenesisBlock() {
+      return new Block('First block in the chain - Genesis block');
+   }
+
+   // addBlock method
    addBlock(newBlock) {
+      newBlock.height = this.chain.length;
+      newBlock.timeStamp = new Date().getTime().toString().slice(0, -3);
+      if (this.chain.length > 0) {
+         // previous block hash
+         newBlock.previousHash = this.chain[this.chain.length - 1].hash;
+      }
+      // SHA256 requires a string of data
+      newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+      // add block to chain
       this.chain.push(newBlock);
    }
 }
+
+let blockchain = new Blockchain();
+
+blockchain.addBlock(new Block('test 1'));
+blockchain.addBlock(new Block('test 2'));
+blockchain.addBlock(new Block('test 3'));
+blockchain.addBlock(new Block('test 4'));
+console.log(blockchain.chain);
